494e3b02f27b8b1ab59974a330338ae022955ad4
Ashu <ashu@gmail.com>
2025-02-12 22:15:25 +0530

    modified controller

--- Project-1/src/main/java/com/javaproject/controller/Controller.java
+++ Project-1/src/main/java/com/javaproject/controller/Controller.java
@@ -1,26 +1,29 @@
 package com.javaproject.controller;
 
 import com.javaproject.dtos.User;
+import com.javaproject.service.IMyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/project-1")
+@RequestMapping("/api/project_1")
 public class Controller {
 
     @Autowired
-    private User user;
+    private IMyService myservice;
 
     @PostMapping("/userdetails")
     public ResponseEntity<String> createUser(@RequestBody User user) {
-
+        myservice.createUser(user);
         return ResponseEntity.ok().body("SUCCESS");
     }
 
     @GetMapping("/getuserdetails")
-    public ResponseEntity<User> getUserDetails() {
-
+    public ResponseEntity<List<User>> getUserDetails() {
+        List<User> user = myservice.getUser();
         return ResponseEntity.ok().body(user);
     }
 
