49f3453719b5be6e4a4e6d21c3dadea7fe51ada7
Ashu <ashu@gmail.com>
2025-05-05 11:51:53 +0530

    add Insert Query

--- Project-1/src/main/java/com/javaproject/mapper/UserMapper.java
+++ Project-1/src/main/java/com/javaproject/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.javaproject.mapper;
 
 import com.javaproject.dtos.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
 
+    @Insert("insert into user(name,email,pass,id) values(#{name},#{email},#{pass},#{id})")
     void createUser(User user);
 
     @Select("select * from user")
